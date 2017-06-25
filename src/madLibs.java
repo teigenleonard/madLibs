336817210613d308cf27d1297d5bfce9292369f5
Teigen Leonard <teigenl@gmail.com>
2017-06-24 22:38:41 -0500

    added conditional statements

--- src/madLibs.java
+++ src/madLibs.java
@@ -7,27 +7,48 @@ public class madLibs
 {
     public static void main(String args[])
     {
+        String ageAsString;
         String name;
         String adjective;
         String noun;
         String adverb;
         String verb;
+        boolean badWord;
 
         Scanner foo = new Scanner(System.in);
+
+        System.out.printf("How old are you?");
+        ageAsString = foo.nextLine();
+        int age = Integer.parseInt(ageAsString);
+        if (age < 13)
+        {
+            System.out.printf("You are not old enough to play this game");
+            System.exit(0);
+        }
+
         System.out.printf("Enter a name:\n");
         name = foo.nextLine();
         System.out.printf("Enter an adjective:\n");
         adjective = foo.nextLine();
-        System.out.printf("Enter a noun:\n");
-        noun = foo.nextLine();
+
+        do
+        {
+            System.out.printf("Enter a noun:\n");
+            noun = foo.nextLine();
+            badWord = (noun.equalsIgnoreCase("dork") || noun.equalsIgnoreCase("jerk"));
+            if (badWord)
+            {
+                System.out.printf("C'mon, you can do better than that. Try again! \n\n");
+            }
+        } while (badWord);
         System.out.printf("Enter an adverb:\n");
         adverb = foo.nextLine();
         System.out.printf("Enter a verb ending in -ing:\n");
         verb = foo.nextLine();
 
-        System.out.printf("My Mad Libs\n _____________________ \n");
-        System.out.printf("%s is a %s %s", name, adjective, noun);
-        System.out.printf("All day and all night they are %s %s", adverb, verb);
+        System.out.printf("My Mad Libs\n _____________________ \n\n");
+        System.out.printf("%s is a %s %s.", name, adjective, noun);
+        System.out.printf("  All day and all night they are %s %s.", adverb, verb);
 
 
     }
